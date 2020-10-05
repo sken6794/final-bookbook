473b37d1a4e1ec3a69bc4ddcab49b75bbf022171
skyrius <skyrius@naver.com>
2020-10-05 18:29:14 +0900

    test-1

--- bookbook/src/main/java/site/itwill/controller/HomeController.java
+++ bookbook/src/main/java/site/itwill/controller/HomeController.java
@@ -26,6 +26,7 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		System.out.println("HI");
+		System.out.println("수정가능해??");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
