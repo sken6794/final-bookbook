62d2a3be2126e352d1e7d8af7f47b3fe3fe54621
haeme <haeme@LAPTOP-7N2DGK76>
2020-10-05 19:11:50 +0900

    수정수정

--- bookbook/src/main/java/site/itwill/controller/HomeController.java
+++ bookbook/src/main/java/site/itwill/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+//«ÿπÃ
 //πŒ√∂-2
 @Controller
 public class HomeController {
