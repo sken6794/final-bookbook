d8daa9605abb3c1c3a4395c4e56c3a17a6eec0d9
min <min@Min1031>
2020-10-05 18:43:27 +0900

    fdsa

--- bookbook/src/main/java/site/itwill/controller/HomeController.java
+++ bookbook/src/main/java/site/itwill/controller/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-//นฮรถ
+//นฮรถ-2
 @Controller
 public class HomeController {
 	
